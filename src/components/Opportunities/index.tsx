af915bc5a8a255fb841bc70957889c63cb4cebdf
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-05-04 06:13:23 +0000

    add: opportunity component

---
+++ src/components/Opportunities/index.tsx
@@ -0,0 +1,30 @@
+import { OpportunityType } from "@/types";
+import styles from "./index.module.css";
+
+interface Props {
+  opportunity: OpportunityType;
+}
+
+export default function Opportunity({ opportunity }: Props) {
+  return (
+    <div className={styles["opportunity-container"]}>
+      <h3>{opportunity.title}</h3>
+      {Object.keys(opportunity).map((key) => (
+        <>
+          {key !== "title" && (
+            <div key={key} className={styles["opportunity-item"]}>
+              <strong>{key}:</strong>
+              <pre>
+                {JSON.stringify(
+                  opportunity[key as keyof OpportunityType],
+                  null,
+                  4,
+                )}
+              </pre>
+            </div>
+          )}
+        </>
+      ))}
+    </div>
+  );
+}
