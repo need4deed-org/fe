9190b7323448c0813452e7cd64d0c9cb0413e265
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-05-04 06:09:52 +0000

    add: data structure

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface OpportunityType {
+  id: string;
+  title: string;
+  description: string;
+  location: string;
+  start_date: string;
+  end_date: string;
+  organization: string;
+  contact: string;
+  website: string;
+}
