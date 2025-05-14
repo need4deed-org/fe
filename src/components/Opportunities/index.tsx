0f5fe14e65c63107add309595a7fa756e498db61
Arturas Mickiewicz <arturas.mickiewicz@need4deed.org>
2025-05-14 18:17:46 +0000

    more functional

--- src/components/Opportunities/index.tsx
+++ src/components/Opportunities/index.tsx
@@ -9,21 +9,17 @@ export default function Opportunity({ opportunity }: Props) {
   return (
     <div className={styles["opportunity-container"]}>
       <h3>{opportunity.title}</h3>
-      {Object.keys(opportunity).map((key) => (
-        <>
-          {key !== "title" && (
-            <div key={key} className={styles["opportunity-item"]}>
-              <strong>{key}:</strong>
-              <pre>
-                {JSON.stringify(
-                  opportunity[key as keyof OpportunityType],
-                  null,
-                  4,
-                )}
-              </pre>
-            </div>
-          )}
-        </>
+      {Object.keys(opportunity).filter((key) => key !== "title").map((key) => (
+        <div key={key} className={styles["opportunity-item"]}>
+          <strong>{key}:</strong>
+          <pre>
+            {JSON.stringify(
+              opportunity[key as keyof OpportunityType],
+              null,
+              4,
+            )}
+          </pre>
+        </div>
       ))}
     </div>
   );
