e20c696b15efe06caec0f34365740d410b37a1ec
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-03 11:18:08 +0000

    add: api/ to url, dunno why

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [{ source: "/api/:path*", destination: "http://vmpub:5000/:path*" }];
+  },
 };
 
 export default nextConfig;
