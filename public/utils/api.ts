8edb8832aaabb84ba141ee0e449f71b11344b1fb
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-08 10:55:29 +0000

    add: hook for managing auth token

--- public/utils/api.ts
+++
@@ -1,9 +0,0 @@
-const keyToken = "backend-token";
-
-export function setToken(token: string) {
-  window.localStorage.setItem(keyToken, token);
-}
-
-export function getToken() {
-  return window.localStorage.getItem(keyToken);
-}
