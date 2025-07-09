78a9da2100322d659c3d2a4508c97cb80066921b
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-09 10:51:28 +0000

    update: use cookie instead of token

--- src/app/persons/Persons.tsx
+++ src/app/persons/Persons.tsx
@@ -1,40 +1,34 @@
 "use client";
 import { useEffect, useState } from "react";
 
-import useToken from "@/hooks/auth/useToken";
 import styles from "./page.module.css";
 import Person, { PersonType } from "./Person";
 
 export default function Persons() {
   const [persons, setPersons] = useState<PersonType[]>([]);
-  const { token } = useToken();
 
   useEffect(() => {
-    if (token) {
-      console.log("DEBUG:Persons:token", token);
-      fetch("http://vmpub:5000/person", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
+    fetch("/api/person", {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          console.error(`HTTP error! status: ${res.status}`);
+          return [];
+        }
       })
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            console.error(`HTTP error! status: ${res.status}`);
-            return [];
-          }
-        })
-        .then(({ data }) => {
-          console.log("DEBUG:Persons:data", data);
-          setPersons(data);
-        })
-        .catch((err) => {
-          console.error("Error fetching persons:", err);
-        });
-    }
-  }, [token]);
+      .then(({ data }) => {
+        setPersons(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching persons:", err);
+      });
+  }, []);
 
   return (
     <div className={styles["list-container"]}>
