2c7b6b5238589e3e7f223480171fd3f24410b85b
arturasmckwcz <arturasmckwcz@gmail.com>
2025-07-30 10:39:17 +0000

    add: stubs

---
+++ src/app/volunteer-form/page.tsx
@@ -0,0 +1,5 @@
+import VolunteerForm from "@/components/forms/volunteer-form";
+
+export default function VolunteerFormPage() {
+  return <VolunteerForm />;
+}
---
+++ src/components/forms/volunteer-form/index.tsx
@@ -0,0 +1,3 @@
+export default function VolunteerForm() {
+  return <div>VolunteerForm</div>;
+}
