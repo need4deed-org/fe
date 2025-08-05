551732f005186dc078e25fe1998f2640ff72748a
aricionur <aricionur@gmail.com>
2025-08-05 13:50:25 +0100

    Add Volunteers initial header component, core search component, mixin for hypened text

---
+++ src/components/Volunteers/Header.tsx
@@ -0,0 +1,117 @@
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { Heading2, Heading4 } from "../styled/text";
+import { Search } from "../core/common";
+import FiltersButton from "./FiltersButton";
+import useScreenType from "../../hooks/useScreenType";
+import { ScreenTypes } from "../../config/types";
+import ResultsFound from "./ResultsFound";
+import { hyphenationStyles } from "../styled/mixins";
+import { CardsFilter } from "./types";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--opportunities-header-title-tabs-gap);
+`;
+
+const TabsSearchBarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--opportunities-header-tabs-searchbar-gap);
+`;
+
+const TabsSectionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: var(--opportunities-header-tabs-section-width);
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: var(--opportunities-header-tabs-gap);
+`;
+
+const SearchBarSectionContainer = styled.div`
+  display: flex;
+  flex-direction: var(--opportunities-header-searchbar-flex-direction);
+  justify-content: space-between;
+  gap: 12px;
+`;
+interface TabHeadingProps {
+  isSelected: boolean;
+}
+
+const TabHeading = styled(Heading4)<TabHeadingProps>`
+  cursor: pointer;
+  border-bottom: ${(props) =>
+    props.isSelected ? "var(--opportunities-header-tabs-border-bottom) solid currentColor" : "none"};
+  padding-bottom: ${(props) => (props.isSelected ? "var(--opportunities-header-tabs-padding-bottom)" : "0")};
+`;
+
+interface Props {
+  numOfOpportunities: number;
+  onSearchInputChange: (input: string) => void;
+  tabs: string[];
+  selectedTabIndex: number;
+  setSelectedTabIndex: (index: number) => void;
+  setIsFiltersOpen: (isOpen: boolean) => void;
+  cardsFilter?: CardsFilter;
+}
+
+const HyphenatedHeading2 = styled(Heading2)`
+  ${hyphenationStyles}
+`;
+
+export default function Header({
+  numOfOpportunities,
+  onSearchInputChange,
+  selectedTabIndex,
+  setSelectedTabIndex,
+  tabs,
+  setIsFiltersOpen,
+  cardsFilter,
+}: Props) {
+  const { t } = useTranslation();
+  const screenSize = useScreenType();
+  const isMobile = screenSize === ScreenTypes.MOBILE;
+  return (
+    <HeaderContainer>
+      <HyphenatedHeading2>{t("dashboard.volunteers.header")}</HyphenatedHeading2>
+
+      <TabsSearchBarContainer>
+        <TabsSectionContainer>
+          <Tabs>
+            {tabs.map((tab, index) => (
+              <TabHeading key={tab} onClick={() => setSelectedTabIndex(index)} isSelected={selectedTabIndex === index}>
+                {tab}
+              </TabHeading>
+            ))}
+          </Tabs>
+
+          {isMobile ? (
+            <FiltersButton setIsFiltersOpen={setIsFiltersOpen} />
+          ) : (
+            <ResultsFound numOfOpportunities={numOfOpportunities} />
+          )}
+        </TabsSectionContainer>
+
+        <SearchBarSectionContainer>
+          <Search
+            placeHolder={`${t("opportunityPage.searchPlaceHolder")} ...`}
+            onInputChange={onSearchInputChange}
+            width="var(--dashboard-volunteers-header-searchbar-width)"
+            cardsFilter={cardsFilter}
+          />
+          {isMobile ? (
+            <ResultsFound numOfOpportunities={numOfOpportunities} />
+          ) : (
+            <FiltersButton setIsFiltersOpen={setIsFiltersOpen} />
+          )}
+        </SearchBarSectionContainer>
+      </TabsSearchBarContainer>
+    </HeaderContainer>
+  );
+}
--- src/components/Volunteers/Volunteers.tsx
+++ src/components/Volunteers/Volunteers.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import styled from "styled-components";
 import { SubPageLayout } from "../Layout/subPageLayout";
+import Header from "./Header";
+import { useTranslation } from "react-i18next";
+
+const VolunteersContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: var(--dashboard-volunteers-container-padding);
+  padding: var(--dashboard-volunteers-container-gap);
+`;
 
 export function Volunteers() {
-  return <SubPageLayout>Volunteers...</SubPageLayout>;
+  const { t } = useTranslation();
+
+  const tabs = [t("opportunityPage.tabs.tab1"), t("opportunityPage.tabs.tab2")];
+
+  return (
+    <SubPageLayout background="var(--color-white)">
+      <VolunteersContainer>
+        <Header tabs={tabs} />
+      </VolunteersContainer>
+    </SubPageLayout>
+  );
 }
 
 export default Volunteers;
