cd231f20382b5a0a00ef31fa4f53c6d162627244
aricionur <aricionur@gmail.com>
2025-08-29 11:29:39 +0100

    status tags div

--- src/components/Dashboard/Volunteers/VolunteerCard.tsx
+++ src/components/Dashboard/Volunteers/VolunteerCard.tsx
@@ -17,23 +17,55 @@ import { Volunteer } from "./types";
 // import { formatAccompanyingDate } from "./utils";
 // import { hyphenationStyles } from "../styled/mixins";
 import { BaseCard } from "@/components/styled/container";
-import { Heading3 } from "@/components/styled/text";
+import { Heading3, Paragraph } from "@/components/styled/text";
 import { hyphenationStyles } from "@/components/styled/mixins";
+import { HourglassIcon, SparkleIcon } from "@phosphor-icons/react";
 
 const Card = styled(BaseCard)`
   background-color: var(--color-orchid-subtle);
-  /* background-color: green; */
-  width: 296px;
-  height: 400px;
-  /* height: auto; */
-  gap: 20px;
-  padding: 0 20px 24px 20px;
+  width: var(--dashboard-volunteers-card-width);
+  height: var(--dashboard-volunteers-card-height);
+  gap: var(--dashboard-volunteers-card-gap);
+  padding: var(--dashboard-volunteers-card-padding);
   transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
   cursor: pointer;
 
   &:hover {
     background-color: var(--color-orchid);
   }
+`;
+
+const StatusTagsDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: var(--dashboard-volunteers-card-status-tags-div-gap);
+  margin-top: var(--dashboard-volunteers-card-status-tags-div-margin-top);
+`;
+
+const StatusDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: var(--color-white);
+  height: var(--dashboard-volunteers-card-status-div-height);
+  border-bottom-left-radius: var(--dashboard-volunteers-card-status-div-bordor-bottom);
+  border-bottom-right-radius: var(--dashboard-volunteers-card-status-div-bordor-bottom);
+  gap: var(--dashboard-volunteers-card-status-div-gap);
+  padding: var(--dashboard-volunteers-card-status-div-padding);
+`;
+
+const TagDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: var(--color-green-200);
+  height: var(--dashboard-volunteers-card-status-div-height);
+  border-bottom-left-radius: var(--dashboard-volunteers-card-status-div-bordor-bottom);
+  border-bottom-right-radius: var(--dashboard-volunteers-card-status-div-bordor-bottom);
+  padding: var(--dashboard-volunteers-card-tag-div-padding);
+  gap: var(--dashboard-volunteers-card-tag-div-gap);
 `;
 
 const LanguagesContainer = styled.div`
@@ -129,6 +161,20 @@ Props) {
 
   return (
     <Card>
+      <StatusTagsDiv>
+        <StatusDiv>
+          <HourglassIcon size={18} color="var(--color-red-500)" />
+          <Paragraph fontWeight={600} fontSize="14px" lineheight="20px" color="var(--color-red-500)">
+            Pending Review
+          </Paragraph>
+        </StatusDiv>
+        <TagDiv>
+          <Paragraph fontWeight={500} fontSize="18px" lineheight="18px" color="var(--color-midnight)">
+            NEW
+          </Paragraph>
+          <SparkleIcon size={18} color="var(--color-midnight)" />
+        </TagDiv>
+      </StatusTagsDiv>
       {/* <IconDiv>{iconNameMap[iconName]}</IconDiv>
       <HyphenatedHeading3 lang={i18n.language}>{title}</HyphenatedHeading3>
       {vo && <Paragraph>{voInformation}</Paragraph>}
