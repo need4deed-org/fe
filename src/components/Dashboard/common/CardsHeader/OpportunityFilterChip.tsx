98f8fa16a31359c6a5bee51ab257094fc9417258
HANS KREBS <hans.krebs@me.com>
2026-03-07 17:15:57 +0100

    #185: Refactors

--- src/components/Dashboard/common/CardsHeader/OpportunityFilterChip.tsx
+++ src/components/Dashboard/common/CardsHeader/OpportunityFilterChip.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import styled from "styled-components";
-import { ShootingStarIcon, XIcon } from "@phosphor-icons/react";
 import { Paragraph } from "@/components/styled/text";
+import { ShootingStarIcon, XIcon } from "@phosphor-icons/react";
+import Image from "next/image";
+import { AvatarWrapper, DefaultAvatarWrapper, OpportunityChip, XIconDiv } from "./styles";
 
 type Props = {
   name: string;
@@ -38,47 +38,3 @@ export function OpportunityFilterChip({ name, avatarUrl, onRemove }: Props) {
     </OpportunityChip>
   );
 }
-
-const AvatarWrapper = styled.div`
-  position: relative;
-  width: var(--opportunity-filter-chip-avatar-size);
-  height: var(--opportunity-filter-chip-avatar-size);
-  border-radius: 50%;
-  overflow: hidden;
-  flex-shrink: 0;
-`;
-
-const DefaultAvatarWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--opportunity-filter-chip-default-avatar-bg);
-`;
-
-const OpportunityChip = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  height: var(--cards-header-filter-item-height);
-  gap: var(--cards-header-filter-item-gap);
-  border-radius: var(--cards-header-filter-item-border-radius);
-  padding: var(--cards-header-filter-item-padding);
-  background-color: var(--opportunity-filter-chip-bg);
-`;
-
-const XIconDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: var(--cards-header-filter-item-icon-div-size);
-  height: var(--cards-header-filter-item-icon-div-size);
-  border-radius: var(--cards-header-filter-item-icon-div-border-radius);
-  cursor: pointer;
-
-  &:hover {
-    background-color: var(--opportunity-filter-chip-x-hover-bg);
-  }
-`;
