901267ee0b3768df94a4e5260c08dace675c97d7
arturasmckwcz <arturasmckwcz@gmail.com>
2026-04-10 09:53:59 +0000

    add: types

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -103,6 +103,9 @@ export function formatDateTime(input: string | Date | undefined): string | undef
  * @param promise The Promise to execute.
  * @returns A Promise that resolves to a Result tuple.
  */
+type Success<T> = readonly [T, null];
+type Failure<E = Error> = readonly [null, E];
+type Result<T, E = Error> = Success<T> | Failure<E>;
 export async function tryCatch<T, E = Error>(promise: Promise<T>): Promise<Result<T, E>> {
   try {
     const result = await promise;
