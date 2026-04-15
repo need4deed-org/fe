8acc74746f0565ae1338c916f11cc8eaccd71798
Rodrigo Louro <rodrigoantuneslouro@gmail.com>
2026-04-15 17:05:16 +0200

    Fix: dashboard button+direct buttons to forms+ testimonials #321
    Fix: dashboard button+direct buttons to forms+ testimonials #321

--- src/app/[lang]/dashboard/page.tsx
+++ src/app/[lang]/dashboard/page.tsx
@@ -1,3 +1,17 @@
-export default function DashboardLandingPage() {
-  return <div>Welcome to your Dashboard!</div>;
+import { Landing } from "@/components/Dashboard/Landing";
+import { TestimonialsSection } from "@/components/Testimonials";
+import { Lang } from "need4deed-sdk";
+
+type Props = {
+  params: Promise<{ lang: string }>;
+};
+
+export default async function DashboardLandingPage({ params }: Props) {
+  const { lang } = await params;
+  return (
+    <>
+      <Landing />
+      <TestimonialsSection lang={lang as Lang} />
+    </>
+  );
 }
