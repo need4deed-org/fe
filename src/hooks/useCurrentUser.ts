fcaaa0ee0bee554acc550cd518b7897758952012
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-04-17 13:26:13 +0200

    removes unneeded useeffect

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -2,7 +2,6 @@ import { apiPathMe, cacheTTL } from "@/config/constants";
 import { useGetQuery } from "@/hooks";
 import { getCookie } from "@/utils/helpers";
 import { ApiUserGet } from "need4deed-sdk";
-import { useEffect } from "react";
 
 export const useCurrentUser = (enabled?: boolean) => {
   const hasAuthHint = getCookie("is_logged_in") === "true";
