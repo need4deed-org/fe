0a86e2f79300c2be5c3fc471e4005cdf432f9412
ivannissimrch <ivannissimrch@gmail.com>
2026-08-04 06:52:26 -0400

    add labels for appreciation types added in sdk 0.0.139

--- src/components/Dashboard/Profile/sections/Appreciation/utils/translations.ts
+++ src/components/Dashboard/Profile/sections/Appreciation/utils/translations.ts
@@ -1,17 +1,16 @@
 import { TFunction } from "i18next";
 import { VolunteerStateAppreciationType } from "need4deed-sdk";
 
-export const getAppreciationTypeLabel = (
-  t: TFunction,
-  type: VolunteerStateAppreciationType,
-): string => {
+export const getAppreciationTypeLabel = (t: TFunction, type: VolunteerStateAppreciationType): string => {
   const map: Record<VolunteerStateAppreciationType, string> = {
-    [VolunteerStateAppreciationType.T_SHIRT]:
-      "dashboard.appreciationSection.typeOptions.tshirt",
-    [VolunteerStateAppreciationType.TOTE_BAG]:
-      "dashboard.appreciationSection.typeOptions.toteBag",
-    [VolunteerStateAppreciationType.BENEFIT_CARD]:
-      "dashboard.appreciationSection.typeOptions.benefitCard",
+    [VolunteerStateAppreciationType.T_SHIRT]: "dashboard.appreciationSection.typeOptions.tshirt",
+    [VolunteerStateAppreciationType.TOTE_BAG]: "dashboard.appreciationSection.typeOptions.toteBag",
+    [VolunteerStateAppreciationType.BENEFIT_CARD]: "dashboard.appreciationSection.typeOptions.benefitCard",
+    [VolunteerStateAppreciationType.NEED4DEED_CERTIFICATE]:
+      "dashboard.appreciationSection.typeOptions.need4deedCertificate",
+    [VolunteerStateAppreciationType.CAP]: "dashboard.appreciationSection.typeOptions.cap",
+    [VolunteerStateAppreciationType.NOTEBOOK]: "dashboard.appreciationSection.typeOptions.notebook",
+    [VolunteerStateAppreciationType.CITY_CERTIFICATE]: "dashboard.appreciationSection.typeOptions.cityCertificate",
   };
   return t(map[type] || type);
 };
